0cc5633774c36f54163dbf670c3c738a85ce712e
Muhammad Luqmaan <96055301+muhammadluqmaan@users.noreply.github.com>
2022-02-25 14:13:47 +0700

    Add files via upload

---
+++ pertemuan10/functions.php
@@ -0,0 +1,17 @@
+<?php 
+
+// Koneksi ke database
+$conn = mysqli_connect("localhost", "root", "", "phpdasar");
+
+
+function query($query) {
+    global $conn;
+    $result = mysqli_query($conn, $query);
+    $rows = [];
+    while( $row = mysqli_fetch_assoc($result) ) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
+?>
---
+++ pertemuan10/tambah.php
@@ -0,0 +1,75 @@
+<?php
+
+$conn = mysqli_connect("localhost", "root", "", "phpdasar");
+
+// cek apakah tombol submit sudah ditekan atau belum
+if( isset($_POST["submit"]) ) {
+    // ambil data dari tiap elemen dalam form
+    $processor = $_POST["processor"];
+    $ram = $_POST["ram"];
+    $vga = $_POST["vga"];
+    $monitor = $_POST["monitor"];
+    $gambar = $_POST["gambar"];
+
+    // query insert data
+    $query = "INSERT INTO spesifikasi
+                VALUES
+                ('', '$processor', '$ram', '$vga', '$monitor', '$gambar')
+                ";
+    mysqli_query($conn, $query);
+
+    // cek apakah data berhasil di tambahkan atau tidak
+    if( mysqli_affected_rows($conn) > 0 ) {
+        echo "Data Berhasil Ditambahkan!";
+    } else {
+        echo "Data Gagal Ditambahkan! <br>";
+        echo mysqli_error($conn);
+    }
+
+}   
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah data mahasiswa</title>
+</head>
+<body>
+    <h1>Tambah data mahasiswa</h1>
+
+    <form action="" method="post">
+
+        <ul>
+            <li>
+                <label for="processor">Processor :</label>
+                <input type="text" name="processor" id="processor">
+            </li>
+            <li>
+                <label for="ram">RAM :</label>
+                <input type="text" name="ram" id="ram">
+            </li>
+            <li>
+                <label for="vga">VGA :</label>
+                <input type="text" name="vga" id="vga">
+            </li>
+            <li>
+                <label for="monitor">Monitor :</label>
+                <input type="text" name="monitor" id="monitor">
+            </li>
+            <li>
+                <label for="gambar">Gambar :</label>
+                <input type="text" name="gambar" id="gambar">
+            </li>
+            <li>
+                <button type="submit" name="submit">Tambah Data!</button>
+            </li>
+        </ul>
+
+    </form>
+
+</body>
+</html>
