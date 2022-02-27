fa5bd3b0eecbeb45414ed3c05f0d4496ab681353
Muhammad Luqmaan <96055301+muhammadluqmaan@users.noreply.github.com>
2022-02-27 10:23:42 +0700

    Add files via upload

---
+++ pertemuan12/functions.php
@@ -0,0 +1,83 @@
+<?php 
+
+// Koneksi ke database
+$conn = mysqli_connect("localhost", "root", "", "phpdasar");
+
+
+function query($query) {
+    global $conn;
+    $result = mysqli_query($conn, $query);
+    $rows = [];
+    while( $row = mysqli_fetch_assoc($result) ) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
+function tambah($data) {
+    global $conn;
+
+    // ambil data dari tiap element dalam form
+    $processor = htmlspecialchars($data["processor"]);
+    $ram = htmlspecialchars($data["ram"]);
+    $vga = htmlspecialchars($data["vga"]);
+    $monitor = htmlspecialchars($data["monitor"]);
+    $gambar = htmlspecialchars($data["gambar"]);
+
+    // query insert data
+    $query = "INSERT INTO spesifikasi
+                VALUES
+                ('', '$processor', '$ram', '$vga', '$monitor', '$gambar')
+                ";
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
+
+function hapus($id) {
+    global $conn;
+    mysqli_query($conn, "DELETE FROM spesifikasi WHERE id = $id");
+
+    return mysqli_affected_rows($conn);
+}
+
+function ubah($data) {
+    global $conn;
+    
+    // ambil data dari tiap elemen dalam form
+    $id = $data["id"];
+    $processor = htmlspecialchars($data["processor"]);
+    $ram = htmlspecialchars($data["ram"]);
+    $vga = htmlspecialchars($data["vga"]);
+    $monitor = htmlspecialchars($data["monitor"]);
+    $gambar = htmlspecialchars($data["gambar"]);
+
+    // query insert data
+    $query = "UPDATE spesifikasi SET 
+               processor = '$processor',
+               ram = '$ram',
+               vga = '$vga',
+               monitor = '$monitor',
+               gambar = '$gambar'
+              WHERE id = $id
+             ";
+    mysqli_query($conn, $query);
+
+
+    return mysqli_affected_rows($conn);
+}
+
+function cari($keyword) {
+    $query = "SELECT * FROM spesifikasi
+              WHERE
+              processor LIKE '%$keyword%' OR
+              ram LIKE '%$keyword%' OR
+              vga LIKE '%$keyword%' OR
+              monitor LIKE '%$keyword%'
+             ";
+
+    return query($query);
+}
+
+
+?>
